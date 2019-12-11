0bed91a549f9e302f8cb700c34ec0221ad3af5e5
abrowning14 <adam.browning@verizonmedia.com>
2019-12-11 15:45:26 +0200

    cache server working localhost

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # prebid-cache-server-demo
 node.js cache server for prebid.js
 
-
-
+1. POST to url with JSON payload of obj
+    {
+        "puts": [
+            {
+                "type": "xml",
+                "value":"VAST TAG",
+                "ttlseconds" 100
+            }
+        ] //array of bids
+    }
+2. Each bid is stored and given a uuid
+3. all uuid's are returned in object
+    {"responses":[
+            {"uuid":"df2051dc-af91-4126-8440-c0219981e120"},
+            {"uuid":"random-number"}
+        ]
+    }
--- index.js
+++ index.js
@@ -1,81 +1,28 @@
-const express = require('express')
+const express = require('express');
 const app = express();
-const morgan = require('morgan')
-const marker = require('@ajar/marker');
-​
-//Middleware
-app.use(morgan('dev'));
+const NodeCache = require( "node-cache" );
+const createId = require('uuid/v1');
+
 app.use(express.json());
-​
-​
-//Routing
+
+const myCache = new NodeCache();
+
+//POST
+app.post('/',  (req, res) => {
+
+    const responses = new Array;
+    for (item of req.body.puts) {
+        const uuid = createId();
+        myCache.set(uuid, item, item.ttlseconds);
+        responses.push({"uuid": uuid});
+    }
+    res.status(200).json({responses});
+})
+
+//GET by UUID
 app.get('/',  (req, res) => {
-    res.status(200).send('Hello Express!')
+    const val = myCache.get(req.query.uuid);
+    res.status(404).send(val ? val.value : "No content stored for uuid=" + req.query.uuid);
 })
-​
-app.get('/users', (req, res) => {
-    res.status(200).send('Get all Users')
-})
-​
-app.post('/users',  (req, res) => {
-    res.status(200).send('create a new User')
-})
-​
-//Query
-//http://localhost:3030/?key1=value1&Key2=value2
-app.get('/query', (req,res) => {
-    marker.blue('key 1 is:',req.query.key1);
-    marker.blue('key 2 is:',req.query.key2);
-    res.status(200).send(`These are the Query string params: ${req.query.key1}`);
-})
-​
-//Params
-//http://localhost:3030/params/tel-aviv/ramat-gan
-app.get('/params/:from/:to/', (req, res) => {
-    marker.blue('URL params are:', req.params.from);
-    marker.blue('URL params are:', req.params.to);
-    res.status(200).send(`The URL params are: ${req.params.from} and ${req.params.to}`);
-})
-​
-//Body
-//http://localhost:3030/body
-app.post('/post', (req, res) => {
-    marker.blue('Body JSON has name of:' , req.body.name)
-    res.status(200).send(`created a new Robot named: ${req.body.name}`)
-})
-​
-//Return JSON
-//http://localhost:3030/json?key1=something
-app.get('/json', (req, res) => {
-    res.status(200).json({
-        "title":"this is the json response",
-        "key1": req.query.key1
-    });
-})
-​
-//Return HTML
-//http://localhost:3030/html?key1=paragraphFromQuery
-app.get('/html', (req, res) => {
-    res.status(200).send(`<h1>TITLE RESPONSE</h1><p>${req.query.key1}</p>`);
-})
-​
-​
-app.get('*',  (req, res) => {
-    res.status(404).send('My 404 not found')
-})
-​
-app.listen(3030,  ()=> {
-    console.log('listening on localhost:3030')
-});
-​
-// install middleware - body-parser morgan - DONE
-// configuring app middleware like so: - DONE
-// app.use( morgan('dev') ); - DONE
-//----------------------------
-// define routes which use - DONE
-//----------------------------
-// req.query - DONE
-// req.params - DONE
-// req.body - DONE
-// return api json dummy response - DONE
-// return html markup dummy response - DONE
+
+app.listen(3030);
