5b3202299d49eb1a0506a699f8dc340b6193828c
abrowning14 <adam.browning@verizonmedia.com>
2019-12-11 18:35:33 +0200

    added cors package

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const NodeCache = require( "node-cache" );
 const createId = require('uuid/v1');
 
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
